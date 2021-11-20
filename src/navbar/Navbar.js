6b5029ce8fd2b75968697f540756ceff46064da0
Wojtek <wojciech.zbrozek@dreamstormstudios.com>
2021-11-20 22:44:15 +0100

    burger onClick slides the drawer in/out

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,9 +1,11 @@
 import './navbar.css'
+import {useEffect} from 'react'
 import Button from '../button/Button';
 import LoggedUser from '../loggedUser/LoggedUser';
 import { AiOutlineMenu } from 'react-icons/ai'
 
 const Navbar = () => {
+    
     return(
         <div id="navbarWrapper">
             <LoggedUser />
@@ -12,7 +14,11 @@ const Navbar = () => {
                 <Button link="/login-form/login" buttonText="LOGIN" />
                 <Button link="/login-form/signin" buttonText="CREATE ACCOUNT" filled={true}/>
             </div>
-            <div id="burgerMenuWrapper">
+            <div id="burgerMenuWrapper" onClick={()=>{
+                document.getElementById("buttonsWrapper").style.right==="0px"?
+                    document.getElementById("buttonsWrapper").style.right="-70vw":
+                    document.getElementById("buttonsWrapper").style.right="0px";
+            }}>
                 <AiOutlineMenu size={40}/>
             </div>
         </div>
